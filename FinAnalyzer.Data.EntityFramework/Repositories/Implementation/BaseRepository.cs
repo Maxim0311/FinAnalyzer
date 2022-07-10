f89eebab010c5824dfc8e516d48eaadae7c45379
maxim0311 <maxim0311@mail.ru>
2022-07-10 21:03:57 +0300

    Добавлен BaseRepository

---
+++ FinAnalyzer.Data.EntityFramework/Repositories/Implementation/BaseRepository.cs
@@ -0,0 +1,31 @@
+﻿using FinAnalyzer.Data.EntityFramework.Repositories.Interfaces;
+using FinAnalyzer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinAnalyzer.Data.EntityFramework.Repositories.Implementation
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly AppDbContext _context;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TEntity?> GetByIdAsync(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<bool> IsExistAsync(int id)
+        {
+            return await _context.Set<TEntity>().AnyAsync(entity => entity.Id == id);
+        }
+    }
+}
---
+++ FinAnalyzer.Data.EntityFramework/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿using FinAnalyzer.Domain.Entities;
+
+namespace FinAnalyzer.Data.EntityFramework.Repositories.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<bool> IsExistAsync(int id);
+
+        Task<TEntity?> GetByIdAsync(int id);
+    }
+}
