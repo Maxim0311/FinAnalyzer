f86a02dd9c62ddb2fd5909a38248a30ac8e13d61
maxim0311 <maxim0311@mail.ru>
2022-12-11 18:24:40 +0300

    Добавлен статический метод Ok() для OperationResult

--- FinAnalyzer.Common/OperationResult.cs
+++ FinAnalyzer.Common/OperationResult.cs
@@ -5,13 +5,20 @@ namespace FinAnalyzer.Common;
 public class OperationResult
 {
     public bool Success => ErrorCode == 0;
+
     public OperationCode ErrorCode { get; set; }
     public string? Message { get; set; }
     public string? StackTrace { get; set; }
 
     public static OperationResult OK => new OperationResult();
-    public static OperationResult Fail(OperationCode errorCode, string? message = null, string? stackTrace = null)
-        => new OperationResult(errorCode, message, stackTrace);
+
+    //public static OperationResult Fail(OperationCode errorCode, string? message = null, string? stackTrace = null)
+    //    => new OperationResult(errorCode, message, stackTrace);
+
+    public static OperationResult<T> Fail<T>(OperationCode errorCode, string? message = null, string? stackTrace = null)
+        => new(errorCode, message, stackTrace);
+
+    public static OperationResult<T> Ok<T>(T result) => new(result);
 
     protected OperationResult()
     {
@@ -31,6 +38,8 @@ public class OperationResult<T> : OperationResult
 
     public static new OperationResult<T> Fail(OperationCode errorCode, string? message = null, string? stackTrace = null)
         => new OperationResult<T>(errorCode, message, stackTrace);
+
+    public static OperationResult<T> Ok(T result) => new OperationResult<T>(result);
 
     public OperationResult()
     {
